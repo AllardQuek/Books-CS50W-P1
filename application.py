1f00b9b8d64917928de16ef3163cc26bba60d853
Allard <AllardQuek@users.noreply.github.com>
2019-09-21 11:18:48 +0800

    completed api route and cleaned up comments

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import requests
 import datetime
 
-from flask import Flask, session, render_template, request, redirect, flash
+from flask import Flask, session, render_template, request, redirect, flash, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -28,6 +28,7 @@ def index():
     # session["user_id"] = user.id ERROR: user in user.id is not defined.
     return render_template("index.html")
 
+
 @app.route("/register", methods=["POST", "GET"])
 def register():
 
@@ -35,15 +36,16 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # Does db.execute fetchall return a list of strings? It returns [('Apple',), ('Allard',)] which is a LIST of 0 or more dict objects, which are keys and values representing a table’s fields and cells, respectively.
-        # type of result is <class 'sqlalchemy.engine.result.RowProxy'>, which cannot be compared to string returned via request.form.get
-
         if db.execute("SELECT username from users WHERE username = :username", {"username": username}).rowcount >= 1:
             return render_template("error.html", message="This username already exists. Sorry!")
 
         db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
                     {"username": username, "password": password})
-        db.commit()
+        try:
+            db.commit()
+        except:
+            return render_template("error.html", message="Could not insert data"), 500
+
         return render_template("register.html")
 
     # Else if method is GET
@@ -60,6 +62,7 @@ def logout():
 def search():
     # If accessed via login home page
     if request.method == "POST":
+
         # Forget any user_id
         session.clear()
 
@@ -86,7 +89,6 @@ def search():
             return redirect("/")
 
 
-
 @app.route("/searchresults", methods=["POST"])
 def searchresults():
     # If no input provided
@@ -96,10 +98,7 @@ def searchresults():
     select = request.form.get("select")
     query = request.form.get("query")
 
-    # Note must use || as + operator is not unique and does not work AND note single quotes
-    # OR if sqltitle = "%" + title + "%"
-    # THEN rows = db.execute("SELECT * FROM books WHERE title LIKE :title", {"title": sqltitle}).fetchall()
-
+    # Use || instead of + operator which is not unique, and use single quotes:
     if select == "isbn":
         try:
             int_query = int(query)
@@ -121,31 +120,27 @@ def searchresults():
 
 @app.route("/bookpage/<isbn>")
 def bookpage(isbn):
-# bookpage function needs to take an argument isbn lest it would not expect a keyword argument
+# Function needs to take an argument isbn lest it would not expect a keyword argument
 
-    # Make sure book with isbn exists
     row = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
-    if row is None:
-        return render_template("error.html", message="No such book!"), 400
-
     reviews = db.execute("SELECT review, username, datetime, rating FROM reviews WHERE isbn = :isbn", {"isbn": isbn }).fetchall()
 
-    # Goodreads reviews data using API
+    # Goodreads review data using API
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "rp5fatbknRT7djHIMLasw", "isbns": isbn})
     ratings_count = res.json()['books'][0]['work_ratings_count']
     average_rating = res.json()['books'][0]['average_rating']
 
-
     return render_template("bookpage.html", row=row, reviews=reviews, ratings_count=ratings_count, average_rating=average_rating)
+
 
 @app.route("/bookpage/<isbn>/submitreview", methods=["POST"])
 def submitreview(isbn):
 
-    # fetchone is important lest ResultProxy object returned instead of RowProxy which has attribute title
+    # Need fetchone() lest ResultProxy object which has no attribute title is returned instead of RowProxy
     user = db.execute("SELECT username FROM users WHERE id = :id", {"id": session["user_id"]}).fetchone()
     book = db.execute("SELECT title FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
 
-    # Check if user has already submitted a review
+    # Check if user has submitted review previously
     if db.execute("SELECT review FROM reviews WHERE title = :title AND username = :username", {"title": book.title, "username": user.username}).rowcount >= 1:
         return render_template("error.html", message="Sorry, you may only submit one review!")
 
@@ -161,11 +156,34 @@ def submitreview(isbn):
     return redirect(f"/bookpage/{isbn}")
 
 
-@app.route("/api/<int:isbn>")
+@app.route("/api/<isbn>")
 def api(isbn):
 
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "rp5fatbknRT7djHIMLasw", "isbns": isbn})
-    if not res:
-        return render_template("error.html", message="isbn number does not exist!"), 404
+    # Make sure isbn exists
+    row = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    if not row:
+        return jsonify({"error": "Invalid isbn"}), 404
 
-    return render_template("api.html", res=res.json())
+    book_details = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    review_count = db.execute("SELECT review FROM reviews WHERE isbn = :isbn", {"isbn": isbn}).rowcount
+
+    # Check if any ratings given so far, if none round function will not work
+    try:
+        average_score_RowProxy = db.execute("SELECT AVG(rating) FROM reviews WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+
+        # Must convert average_score to dict lest its type is RoxProxy which is not JSON serializable
+        d = dict(average_score_RowProxy)
+
+        # Round to 2 decimal places, but still must convert to float lest type Decimal is not JSON serializable
+        average_score = float(round((d['avg']), 2))
+    except:
+        average_score = 0
+
+    return jsonify({
+            "title": book_details["title"],
+            "author": book_details["author"],
+            "year": book_details["year"],
+            "isbn": book_details["isbn"],
+            "review_count": review_count,
+            "average_score": average_score
+    })
